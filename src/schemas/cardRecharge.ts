84f7bbf61c3a4095cfbe87b344d932b6cc8d6e48
iagod <iago.tostes@cubos.io>
2022-04-27 17:22:02 -0300

    Preparando revisão de código

--- src/schemas/cardRecharge.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-const cardRecharge = Joi.object({
-  id: Joi.number().positive().integer().required(),
-  amount: Joi.number().positive().integer().required(),
-});
-
-export default cardRecharge;
--- src/services/employeeService.ts
+++
@@ -1,7 +0,0 @@
-import * as employeeRepository from "../repositories/employeeRepository.js";
-
-export async function findById(id: number) {
-  const employee = await employeeRepository.findById(id);
-  if (!employee) throw "error_not_found";
-  return employee;
-}
